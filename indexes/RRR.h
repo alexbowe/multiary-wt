58996f5bc6422f75214fc02761962a886562954f
alexbowe <bowe.alexander@gmail.com>
2010-10-10 13:48:24 +1100

    skeleteon building for RRR

--- indexes/RRR.h
+++ indexes/RRR.h
@@ -1,44 +1,12 @@
 #ifndef RRR_H
 #define RRR_H
 
-#include "Debug.h"
+#include <vector>
 
 using namespace std;
 
 namespace indexes
 {
-// namespace RRR...
-// RRR_utilities.h
-// GTable
-// typedef tuple<size_t bits, bla *>
-// typedef vector<> row;
-// vector<row>
-
-class RRR
-{
-public:
-    // Might change to char, bool, int, long, etc...
-    typedef uint32_t symbol_t;
-    
-private:
-    const size_t ARITY;
-    const size_t BLOCK_SIZE;
-    const size_t SUPER_BLOCK_FACTOR;
-    
-public:
-    // these will have default values...
-    // Could create hash functions from the first 2...
-    RRR(size_t arity, size_t block_size, size_t superBlockFactor);
-    
-    // NOTE: Tomorrow: RRR.c, build without packing bits first...
-    // CountCube dims: class, offset, (symbol), position
-    
-    // RRR should take bools, chars, unsigned ints 32, and unsigned longs
-    // as input... maybe iterators...
-    SuperBlock build(vector<symbol_t> & sequence);
-    size_t rank(symbol_t symbol, size_t position, RRRSequence & seq) const;
-    // void seal(); // to get rid of the space we use to map the blocks/classes
-};
 
 class RRRSequence
 {
@@ -47,6 +15,33 @@ private:
     // array of c... int...
     // array of o int*
     // prefix sum = int[]?
+    int a;
+};
+
+class RRR
+{
+public:
+    typedef unsigned int symbol_t;
+    typedef unsigned long size_type;
+    
+private:
+    const size_type ARITY;
+    const size_type BLOCK_SIZE;
+    const size_type SUPER_BLOCK_FACTOR;
+    
+    // CountCube dims: class, offset, (symbol), position
+    // Mappers for offset and classes
+    
+public:
+    // these will have default values...
+    // Could create hash functions from the first 2...
+    RRR(size_type arity, size_type block_size, size_type s_block_factor);
+    
+    // RRR should take bools, chars, unsigned ints 32, and unsigned longs
+    // as input... maybe iterators...
+    RRRSequence build(const vector< symbol_t > & v);
+    size_type rank(symbol_t symbol, size_type position, RRRSequence & seq)
+        const;
 };
 
 } // end of namespace
