92d85c02e0114a72649f9a278139b68f3eb091eb
alexbowe <bowe.alexander@gmail.com>
2010-10-10 15:39:27 +1100

    reflecting api

--- tests/RRRTest.cc
+++ tests/RRRTest.cc
@@ -1,20 +1,20 @@
 #include "CppUnitLite/TestHarness.h"
-//#include "indexes/RRR.h"
+#include "indexes/RRR.h"
 #include <vector>
 
 using namespace std;
-//using namespace indexes;
+using namespace indexes;
 
 TEST( RRR, popcount )
 {
-    int arity = 4;
-    int blocksize = 5;
-    int superblocksize = 3;
-    //RRR rrr = RRR(arity, blocksize, superblocksize);
+    unsigned int arity = 4;
+    unsigned int blocksize = 5;
+    unsigned int superblocksize = 3;
+    RRR rrr = RRR(arity, blocksize, superblocksize);
     
-    int values[] = {0, 1, 2, 0, 3, 0, 1, 2, 0, 1, 1};
+    //int values[] = {0, 1, 2, 0, 3, 0, 1, 2, 0, 1, 1};
     
-    //rrr.build();
+    //RRRSequence s = rrr.build();
 
     //for (unsigned int i = 0; i < length; i++)
     //{
@@ -22,4 +22,5 @@ TEST( RRR, popcount )
     //    CHECK(wt.rank('b', i) == b_ranks[i]);
     //    CHECK(wt.rank('c', i) == c_ranks[i]);
     //}
+    CHECK(true);
 }
