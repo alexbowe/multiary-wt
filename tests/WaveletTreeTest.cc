aebc2aea92e10fddc111128c62c71c7ed5b40adc
alexbowe <bowe.alexander@gmail.com>
2010-10-14 02:25:22 +1100

    test skeleton, nonfunctional

---
+++ tests/WaveletTreeTest.cc
@@ -0,0 +1,10 @@
+#include "CppUnitLite/TestHarness.h"
+#include "indexes/WaveletTree.h"
+
+using namespace indexes;
+
+TEST( WaveletTree, rank )
+{
+    // need to write the test
+    CHECK(false);
+}
