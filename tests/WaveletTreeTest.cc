0164c73a4a2eb499d39ca4c9dc71e831e0638b72
alexbowe <bowe.alexander@gmail.com>
2010-10-15 01:57:02 +1100

    making the unit test try a few more combinations of arity, blocksize, super block factor

--- tests/WaveletTreeTest.cc
+++ tests/WaveletTreeTest.cc
@@ -12,22 +12,43 @@ using namespace std;
 TEST( WaveletTree, charRank )
 {
     basic_string<char> input = "this is a test string!";
-    CHECK( doCountTest(input, 4, 5, 2) );
+    
+    basic_string<char> alphabet = getAlphabet(input);
+    map<char, CountVect> counters = prepareCountMap(input, alphabet);
+    
+    for (int arity = 2; arity <= 8; arity++)
+        for (int blocksize = 5; blocksize <= 15; blocksize+= 5)
+            for (int sb_fact = 2; sb_fact <= 6; sb_fact+=2)
+                CHECK(doCountTest(input, counters, arity, blocksize, sb_fact));
 }
 
 TEST( WaveletTree, intRank )
 {
     int ints[] = {1, 1, 3, 1, 0, 2, 4};
     basic_string<int> input(ints, ARRAY_SIZE(ints, int));
-    CHECK( doCountTest(input, 4, 5, 2) );
+    
+    basic_string<int> alphabet = getAlphabet(input);
+    map<int, CountVect> counters = prepareCountMap(input, alphabet);
+    
+    for (int arity = 2; arity <= 8; arity*=2)
+        for (int blocksize = 5; blocksize <= 15; blocksize+= 5)
+            for (int sb_fact = 1; sb_fact <= 5; sb_fact+=2)
+                CHECK(doCountTest(input, counters, arity, blocksize, sb_fact));
 }
 
 TEST( WaveletTree, charRankHardcore )
 {
     basic_string<char> input = "this is a test string! It's pretty long so "
-        " I hope it works. Anyway, how was your day? Maybe I should make a"
-        " test text generator too, because this is nothing compared to the"
-        " actual documents I want to use this on. Oh well, here goes..."
-        " Yay! It passed... now fingers crossed for the experiments! :)" ;
-    CHECK( doCountTest(input, 4, 5, 2) );
+        " I hope it works. Maybe I should make a test text generator too, "
+        " because this is nothing compared to the actual documents I want to"
+        " use this on. Oh well, here goes... Yay! It passed... now fingers"
+        " crossed for the experiments! :) Anyway, how was your day?" ;
+        
+    basic_string<char> alphabet = getAlphabet(input);
+    map<char, CountVect> counters = prepareCountMap(input, alphabet);
+    
+    for (int arity = 2; arity <= 8; arity*=2)
+        for (int blocksize = 5; blocksize <= 10; blocksize+= 5)
+            for (int sb_fact = 1; sb_fact <= 5; sb_fact+=2)
+                CHECK(doCountTest(input, counters, arity, blocksize, sb_fact));
 }
