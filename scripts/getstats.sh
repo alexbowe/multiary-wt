fd67f1e8252536d50ab19f9137daf832a56bf85f
alexbowe <bowe.alexander@gmail.com>
2010-10-26 01:30:53 +1100

    script to get csv stats of files

---
+++ scripts/getstats.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo 'type,size,length,sigma'
+for file in $1*/*MB.bwt.1
+do
+    TYPE=`echo $file | sed 's/.*\/\(.*\)\.\([0-9][0-9]MB\)\.bwt\.1/\1/'`
+    SIZE=`echo $file | sed 's/.*\/\(.*\)\.\([0-9][0-9]MB\)\.bwt\.1/\2/'`
+    LENGTH=`grep 'Text Length' $file | awk '{print $4}'`
+    SIGMA=`grep 'Sigma' $file | awk '{print $3}'`
+    echo $TYPE,$SIZE,$LENGTH,$SIGMA
+done
+
+for file in $1*/*MB.swit.bwt.1
+do
+    TYPE=`echo $file | sed 's/.*\/\(.*\)\.\([0-9][0-9]MB\)\.swit\.bwt\.1/\1/'`
+    SIZE=`echo $file | sed 's/.*\/\(.*\)\.\([0-9][0-9]MB\)\.swit\.bwt\.1/\2/'`
+    LENGTH=`grep 'Text Length' $file | awk '{print $4}'`
+    SIGMA=`grep 'Sigma' $file | awk '{print $3}'`
+    echo $TYPE.ints,$SIZE,$LENGTH,$SIGMA
+done
